6139d64e0781e5c666495dabe475c8212e33084b
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-02-20 12:30:11 +0100

    ok

--- js/contact.js
+++ js/contact.js
@@ -5,16 +5,30 @@ document.querySelector('#frmContact').addEventListener('submit', (e) => {
     const email = e.target.txtEmail.value;
     const comments = e.target.txtComments.value;
 
-    const submittedInfo = `
-        Thank you for your message. We will get back to you shortly.
+    // const submittedInfo = `
+    //     Thank you for your message. We will get back to you shortly.
 
-        Registered information:
-        - Name: ${name}
-        - Email: ${email}
-        - Comments: ${comments}
-    `;
+    //     Registered information:
+    //     - Name: ${name}
+    //     - Email: ${email}
+    //     - Comments: ${comments}
+    // `;
 
-    alert(submittedInfo);
+    // alert(submittedInfo);
 
+   const modal =  document.querySelector('#contact_message')
+   modal.querySelector('#name').innerText = name;
+   modal.querySelector('#email').innerText = email;
+   modal.querySelector('#comments').innerText = comments;
+
+modal.showModal();
+modal.focus();
+
+modal.querySelector('.close').addEventListener('click', () => {
+   modal.close();
+});
+modal.addEventListener('close', () => {
     window.location.replace('index.html');
+})
+    // window.location.replace('index.html');
 });
